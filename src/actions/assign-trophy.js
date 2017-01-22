efe67798bed24c843db850999389605d923b6a3b
Tanja Hennis <tmhennis@gmail.com>
2017-01-22 16:23:12 +0100

    november version

---
+++ src/actions/assign-trophy.js
@@ -0,0 +1,8 @@
+// export const ASSIGN_TROPHY = 'ASSIGN_TROPHY'
+//
+// export default (updatedAt) => {
+//   return {
+//     type: ASSIGN_TROPHY,
+//     payload: updatedAt
+//   }
+// }
--- src/actions/plus-one.js
+++ src/actions/plus-one.js
@@ -1,6 +1,21 @@
+import api from '../middleware/api'
+import appLoading from './loading'
+
 export const PLUS_ONE = 'PLUS_ONE'
 
-export default (playerId) => {
+export default (background = false) => {
+  return (dispatch) => {
+    if (background) dispatch(appLoading(true))
+
+    api.patch('player')
+      .then((playerId) => {
+        dispatch(plusOne(playerId))
+        if (background) dispatch(appLoading(false))
+      })
+  }
+}
+
+export const plusOne = (playerId) => {
   return {
     type: PLUS_ONE,
     payload: playerId
